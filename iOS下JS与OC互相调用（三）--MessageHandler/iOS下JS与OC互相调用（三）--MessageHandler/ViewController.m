e0916b9636ee3fa65bff6f2d0277addce5da50de
nanxinlin <18392056542@163.com>
2017-07-11 18:37:23 +0800

    创建

---
+++ iOS下JS与OC互相调用（三）--MessageHandler/iOS下JS与OC互相调用（三）--MessageHandler/ViewController.m
@@ -0,0 +1,130 @@
+//
+//  ViewController.m
+//  iOS下JS与OC互相调用（三）--MessageHandler
+//
+//  Created by 南鑫林 on 2017/7/11.
+//  Copyright © 2017年 nanxinlin. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <WebKit/WebKit.h>
+@interface ViewController ()<WKNavigationDelegate,WKUIDelegate,WKScriptMessageHandler>
+@property (nonatomic, strong) WKWebView *webView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 这是创建configuration 的过程
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    WKPreferences *preferences = [WKPreferences new];
+    preferences.javaScriptCanOpenWindowsAutomatically = YES;
+    preferences.minimumFontSize = 40.0;
+    configuration.preferences = preferences;
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:configuration];
+    NSString *urlStr = [[NSBundle mainBundle] pathForResource:@"index.html" ofType:nil];
+    NSURL *fileURL = [NSURL fileURLWithPath:urlStr];
+    [self.webView loadFileURL:fileURL allowingReadAccessToURL:fileURL];
+    self.webView.navigationDelegate = self;
+    self.webView.UIDelegate = self;
+    [self.view addSubview:self.webView];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    // addScriptMessageHandler 很容易导致循环引用
+    // 控制器 强引用了WKWebView,WKWebView copy(强引用了）configuration， configuration copy （强引用了）userContentController
+    // userContentController 强引用了 self （控制器）
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"ScanAction"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"Location"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"Share"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"Color"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"Pay"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"Shake"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"GoBack"];
+    [self.webView.configuration.userContentController addScriptMessageHandler:self name:@"PlaySound"];
+}
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    // 因此这里要记得移除handlers
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"ScanAction"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"Location"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"Share"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"Color"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"Pay"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"Shake"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"GoBack"];
+    [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"PlaySound"];
+}
+#pragma mark - WKScriptMessageHandler
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
+{
+    NSLog(@"%@",message.name);
+    //    message.body  --  Allowed types are NSNumber, NSString, NSDate, NSArray,NSDictionary, and NSNull.
+    if ([message.name isEqualToString:@"scanClick"]) {
+        NSLog(@"扫一扫");
+    } else if ([message.name isEqualToString:@"Location"]) {
+        NSLog(@"获取定位");
+        [self getLocation];
+
+    } else if ([message.name isEqualToString:@"Color"]) {
+        NSLog(@"修改背景色");
+        [self shareWithParams:message.body];
+    } else if ([message.name isEqualToString:@"Share"]) {
+        NSLog(@"分享");
+        [self shareWithParams:message.body];
+    } else if ([message.name isEqualToString:@"payClick"]) {
+        NSLog(@"支付");
+    } else if ([message.name isEqualToString:@"goBack"]) {
+        NSLog(@"返回");
+    }
+    
+}
+- (void)getLocation
+{
+    // 获取位置信息
+    // 将结果返回给js
+    NSString *jsStr = [NSString stringWithFormat:@"setLocation('%@')",@"广东省深圳市南山区学府路XXXX号"];
+    [self.webView evaluateJavaScript:jsStr completionHandler:^(id _Nullable result, NSError * _Nullable error) {
+        NSLog(@"%@----%@",result, error);
+    }];
+}
+- (void)shareWithParams:(NSDictionary *)tempDic
+{
+    if (![tempDic isKindOfClass:[NSDictionary class]]) {
+        return;
+    }
+    
+    NSString *title = [tempDic objectForKey:@"title"];
+    NSString *content = [tempDic objectForKey:@"content"];
+    NSString *url = [tempDic objectForKey:@"url"];
+    // 在这里执行分享的操作
+    
+    // 将分享结果返回给js
+    NSString *jsStr = [NSString stringWithFormat:@"shareResult('%@','%@','%@')",title,content,url];
+    [self.webView evaluateJavaScript:jsStr completionHandler:^(id _Nullable result, NSError * _Nullable error) {
+        NSLog(@"%@----%@",result, error);
+    }];
+}
+#pragma mark - WKUIDelegate
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提醒" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
